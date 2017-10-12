705879798b7de6fe63572eafc7c76eadad7136dd
sa9sha9 <sa9sha9@gmail.com>
2017-10-12 14:50:27 +0900

    [ADD] yylex()

--- ex01/scanner.cc
+++ ex01/scanner.cc
@@ -24,24 +24,18 @@ using namespace std;
 // 定数の定義
 #define FOREVER	1
 
-/*	          */
 /* 文字判定マクロ */
-/*		  */
-
 // 空白文字なら真
 #define isWhiteSpace(c)	(c == ' ' || c == '\t' || c == '\n')
 
 // ファイル内部だけで有効な関数のプロトタイプ宣言
-
 static int getIdentifier(int c);
 static int getCharacter(void);
 
 // プログラム全体で有効な変数
-
 int lineNo;	// 処理中の行の行番号(scaner.ccとerror.ccで使用)
 
 // ファイル内部だけで有効な変数
-
 static FILE *srcFilePointer;	// 原始プログラムのファイルディスクリプタ
 static int currentChar;		// 先読み文字バッファ
 
@@ -66,7 +60,7 @@ void initializeScanner(char *filename)
     srcFilePointer = fp;
     lineNo = 1;
   }
-  fclose(fp);
+//  fclose(fp);
 }
 
 
@@ -77,8 +71,8 @@ void initializeScanner(char *filename)
 /*         読み込んだ文字に応じてトークンの種類を決定し，*/
 /*         トークンを返す．またトークンの属性があれば    */
 /*         それをyylvalに格納する．                      */
-int yylex()
-{
+int yylex() {
+    string c = "";
  START:
   // 空白の読み飛ばし
   while (isWhiteSpace(currentChar))  {
@@ -87,23 +81,63 @@ int yylex()
   // この時点では currentChar には空白以外の文字が入っている
 
   // 識別子の取得
-  if (/** currentCharが英字か **/)  {
-    return getIdentifier(currentChar);
+  if (isalpha(currentChar)) {
+      return getIdentifier(currentChar);
 
-    /**
-    ここに，以下の字句を読み取る処理とコメントを読み飛ばす処理を書く。
-    （処理する順番は任意でよい）
-    ・加減算演算子（+と-）
-    ・乗除算演算子（*と/と%）
-    ・論理演算子のうち&&と||
-     */
+      /**
+      ここに，以下の字句を読み取る処理とコメントを読み飛ばす処理を書く。
+      （処理する順番は任意でよい）
+      ・加減算演算子（+と-）
+      ・乗除算演算子（*と/と%）
+      ・論理演算子のうち&&と||
+       */
+// 加減算演算子(+, -)
+  } else if (currentChar == Cadd || currentChar == Csubtract) {
+      return ADDOP;
+// 乗除算演算子(*, %)
+  } else if (currentChar == Cmultiply || currentChar == Cmodulo) {
+      return MULOP;
+// 乗除算演算子(/)およびコメントアウト(//)
+  } else if (currentChar == Cdivide) {
+      // 次の文字を先読み
+      currentChar = getCharacter();
 
+      // ここからコメントが始まる
+      if (currentChar == Cdivide) {
+          do {
+              currentChar = getCharacter();
+          } while (currentChar == '\n' || currentChar == EOF);
+
+          // 改行文字まで
+          if (currentChar == '\n') {
+              goto START;
+          }
+          // EOFまで
+          if (currentChar == EOF) {
+              return EOF;
+          }
+      }
+
+      return MULOP;
+// 論理演算子(&&)
+  } else if (currentChar == Cand) {
+      currentChar = getCharacter();
+      if (currentChar == Cand) {
+          return LOGOP;
+      }
+      compileError(EIllegalChar,currentChar,currentChar);
+// 論理演算子(||)
+  } else if (currentChar == Cor) {
+      currentChar = getCharacter();
+      if (currentChar == Cor) {
+          return LOGOP;
+      }
+      compileError(EIllegalChar,currentChar,currentChar);
   // ファイルの終わりを表すEOFを読んだとき
   } else if (currentChar == EOF)  {
     return EOF;
-  }
   // その他の文字は不正な文字なのでエラー
-  else  {
+  } else  {
     compileError(EIllegalChar,currentChar,currentChar);
   }
 }
@@ -113,18 +147,18 @@ int yylex()
 //   c: 識別子の１文字目（英字）
 //   返り値: トークン ID
 //   副作用: yylval.symbol に字句へのポインタを代入
-static int getIdentifier(int c)
-{
-    /**
-    ここで，識別子の字句を読み取る有限オートマトンをシミュレートする．
-    字句を保存するための局所変数を用意すること．
-    字句へのポインタを yylval.symbol に代入し，識別子のトークンを返す．
-     */
-
-  yylval.symbol = ;//グローバル変数 yylval に字句を保存
-                  //yylval.symbol の型は y.tab.h を参照のこと．
-  return ID;
-}
+//static int getIdentifier(int c)
+//{
+//    /**
+//    ここで，識別子の字句を読み取る有限オートマトンをシミュレートする．
+//    字句を保存するための局所変数を用意すること．
+//    字句へのポインタを yylval.symbol に代入し，識別子のトークンを返す．
+//     */
+//
+//  yylval.symbol = ;//グローバル変数 yylval に字句を保存
+//                  //yylval.symbol の型は y.tab.h を参照のこと．
+//  return ID;
+//}
 
 // 文字読み取りモジュール
 //  返り値: ファイルから読んだ文字
