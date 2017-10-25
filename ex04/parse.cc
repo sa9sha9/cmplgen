c36a6aecbba15b8db64d4a4e54c99ca4778d80fb
sa9sha9 <sa9sha9@gmail.com>
2017-10-25 20:53:03 +0900

    Reformat code

--- ex04/parse.cc
+++ ex04/parse.cc
@@ -23,11 +23,17 @@ using namespace std;
 
 // static関数のプロトタイプ宣言
 static bool parseLF(void);
+
 static bool parseLF2(bool);
+
 static bool parseNoImpl(void);
+
 static bool parseNoImpl2(bool);
+
 static bool parseSimpleLF(void);
+
 static void match(int x);
+
 static void parseError(void);
 
 // トークンを保存するstatic変数
@@ -49,20 +55,18 @@ static int token;
 //   引数: なし
 //   返り値: 解析した論理式の真理値
 //   副作用: なし
-bool parse()
-{
-  bool b;
+bool parse() {
+    bool b;
 
-  token = getToken();	// 最初のトークンを先読み
-  b = parseLF();	// 構文解析の開始
+    token = getToken();    // 最初のトークンを先読み
+    b = parseLF();    // 構文解析の開始
 
-  // EOFでなければ余分なトークンが残っていたことになるのでエラー
-  if (token != EOF)  {
-    compileError(ETooMuchTokens,getLexeme(token));
-    // getLexeme() のプロトタイプ宣言は lf-scanner.h にある
-  }
-  else
-    return b;
+    // EOFでなければ余分なトークンが残っていたことになるのでエラー
+    if (token != EOF) {
+        compileError(ETooMuchTokens, getLexeme(token));
+        // getLexeme() のプロトタイプ宣言は lf-scanner.h にある
+    } else
+        return b;
 }
 
 /** ボーナス点を狙うチームはここから parseNoImpl2()の定義までコメントアウトし、
@@ -73,33 +77,32 @@ bool parse()
 //   引数: なし
 //   返り値: 解析した論理式の真理値
 //   副作用: なし
-bool parseLF()
-{
-  bool bl, tmp;
+bool parseLF() {
+    bool bl, tmp;
 
-  switch ( token ) {
-  case NOT:
-    tmp = parseNoImpl();
-    bl = parseLF2( tmp );
-    break;
-  case '(':
-    tmp = parseNoImpl();
-    bl = parseLF2( tmp );
-    break;
-  case TRUE:
-    tmp = parseNoImpl();
-    bl = parseLF2( tmp );
-    break;
-  case FALSE:
-    tmp = parseNoImpl();
-    bl = parseLF2( tmp );
-    break;
-  default:
-    parseError();
-    break;
-  }
+    switch (token) {
+        case NOT:
+            tmp = parseNoImpl();
+            bl = parseLF2(tmp);
+            break;
+        case '(':
+            tmp = parseNoImpl();
+            bl = parseLF2(tmp);
+            break;
+        case TRUE:
+            tmp = parseNoImpl();
+            bl = parseLF2(tmp);
+            break;
+        case FALSE:
+            tmp = parseNoImpl();
+            bl = parseLF2(tmp);
+            break;
+        default:
+            parseError();
+            break;
+    }
 
-  return bl;
+    return bl;
 }
 
 
@@ -107,11 +110,10 @@ bool parseLF()
 //   引数: トークンIMPLの前に現れる論理式の真理値
 //   戻り値: 引数と構文解析の結果から決まる真理値
 //   副作用: なし
-bool parseLF2(bool b)
-{
+bool parseLF2(bool b) {
     bool bl;
 
-    switch(token) {
+    switch (token) {
         case IMPL:
             // IMPL 論理式
             /**
@@ -142,12 +144,11 @@ bool parseLF2(bool b)
 //   引数: なし
 //   返り値: 解析した含意無し論理式の真理値
 //   副作用: なし
-bool parseNoImpl()
-{
+bool parseNoImpl() {
     bool bl;
 
     // 最初はとりあえず単純論理式の展開をする ( first(含意なし論理式)はすべてfirst(単純論理式)が実体だし.. )
-    switch(token) {
+    switch (token) {
         case NOT:
             bl = parseSimpleLF();
             break;
@@ -175,13 +176,12 @@ bool parseNoImpl()
 //   引数: トークンAND, ORの前に現れる論理式の真理値
 //   戻り値: 引数と構文解析の結果から決まる真理値
 //   副作用: なし
-bool parseNoImpl2(bool b)
-{
+bool parseNoImpl2(bool b) {
     bool bl, putty; // puttyはサンドイッチの肉
 
     // todo: C++の論理式の解釈仕様を考慮してやる必要がある？
 
-    switch(token) {
+    switch (token) {
         case AND:
             // OR 単純論理式 含意無し論理式2
             match(AND);
@@ -215,16 +215,15 @@ bool parseNoImpl2(bool b)
 //   引数: なし
 //   返り値: 解析した単純論理式の真理値
 //   副作用: なし
-bool parseSimpleLF()
-{
+bool parseSimpleLF() {
     bool bl;
 
-    switch(token) {
+    switch (token) {
         case NOT:
             // NOT 単純論理式
             match(NOT);
             // 次のtokenは '単純論理式'
-            bl = ! parseSimpleLF(); // 単純論理式の値にNOT演算をする
+            bl = !parseSimpleLF(); // 単純論理式の値にNOT演算をする
             break;
         case '(':
             // ( 論理式 )
@@ -255,33 +254,30 @@ bool parseSimpleLF()
 //   返り値: なし
 //   副作用: トークンが一致したら、token に次のトークンが格納される。
 //           一致しなければエラー終了し、呼び出し側には戻らない。
-void match(int x)
-{
-  if (x == token)  {
-    token = getToken();		// 一致すれば次のトークンを先読み
-  }
-  // EOF を読み込んだ
-  else if (token == EOF) {
-    compileError(EUnexpectedEOF);
-  }
-  // EOFではないが，x と異なるトークンを読み込んだ
-  else
-    compileError(EIllegalToken,getLexeme(x),getLexeme(token));
+void match(int x) {
+    if (x == token) {
+        token = getToken();        // 一致すれば次のトークンを先読み
+    }
+        // EOF を読み込んだ
+    else if (token == EOF) {
+        compileError(EUnexpectedEOF);
+    }
+        // EOFではないが，x と異なるトークンを読み込んだ
+    else
+        compileError(EIllegalToken, getLexeme(x), getLexeme(token));
 }
 
 // 構文解析のエラー処理
 //   引数: なし
 //   返り値: なし
 //   副作用: つねにエラー終了し、呼び出し側には戻らない。
-void parseError()
-{
-  if (token == EOF)  {
-    // EOF のときは，予期しないファイルの終りに遭遇したことを知らせる
-    compileError(EUnexpectedEOF);
-  }
-  else  {
-    // それ以外のときは，単純論理式を始めるトークンでないものを
-    // 読んだことを知らせる
-      compileError(ENotSimpleLF,getLexeme(token));
-  }
+void parseError() {
+    if (token == EOF) {
+        // EOF のときは，予期しないファイルの終りに遭遇したことを知らせる
+        compileError(EUnexpectedEOF);
+    } else {
+        // それ以外のときは，単純論理式を始めるトークンでないものを
+        // 読んだことを知らせる
+        compileError(ENotSimpleLF, getLexeme(token));
+    }
 }
