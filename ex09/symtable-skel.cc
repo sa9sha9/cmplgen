b270790ebd141339e14dbc6e0fcbf5a96bba2e9f
sa9sha9 <sa9sha9@gmail.com>
2017-11-20 11:05:43 +0900

    Copy functions from ex07

--- ex09/symtable-skel.cc
+++ ex09/symtable-skel.cc
@@ -62,26 +62,62 @@ Code *getMainCode()
   return proc->getCode();
 }
 
+
 // 大域変数のための変数エントリを生成し、大域的な記号表に登録する
 // 登録した変数エントリへのポインタを返す
-課題７で作成した関数 addGlobalVariable() の定義をここにペースト（この行は削除すること）
+VarEntry *addGlobalVariable(string name, Type type)
+{
+  // この関数の本体を変更すること
+  return addVariable(name, GlobalVar, type, false, 0, &globalSymTable);
+}
 
 // 配列のための変数エントリを生成し、大域的な記号表に登録する
 // 登録した変数エントリへのポインタを返す
-課題７で作成した関数 addArray() の定義をここにペースト（この行は削除すること）
+VarEntry *addArray(string name, Type type, int size)
+{
+  VarEntry *var = addVariable(name, GlobalVar, type, true, size, &globalSymTable);
+  return var;
+}
 
 // 局所変数のための変数エントリを生成し、局所的な記号表に登録する
 // 登録した変数エントリへのポインタを返す
-課題７で作成した関数 addLocalVariable() の定義をここにペースト（この行は削除すること）
+VarEntry *addLocalVariable(string name, Type type)
+{
+  // この関数の本体を変更すること
+  VarEntry *var = addVariable(name, LocalVar, type, false, 0, &localSymTable);
+  return var;
+}
 
 // 仮引数のための変数エントリを生成し、局所的な記号表に登録する
 // 登録した変数エントリへのポインタを返す
-課題７で作成した関数 addParameter() の定義をここにペースト（この行は削除すること）
+VarEntry *addParameter(string name, Type type)
+{
+  // この関数の本体を変更すること
+  VarEntry *var = addVariable(name,Param, type, false, 0, &localSymTable);
+  return var;
+}
 
 // 識別子名nameと変数の種別vcをもつ変数エントリを生成し、
 // tableが指す記号表にその変数エントリを登録する
 // 登録した変数エントリへのポインタを返す
-課題７で作成した static 関数 addVariable() の定義をここにペースト（この行は削除すること）
+static VarEntry *addVariable(string name, VarClass vc, Type type,
+                             bool array, int size, SymbolTable *table)
+{
+  // この関数の本体を変更すること
+
+  // 同じ名前の識別子が与えられた記号表に存在するかチェック、あればエラー
+  SymbolEntry *symbol = find(name,table);
+  if (symbol != NULL)  {
+    if (symbol->isVariable())
+      compileError(EVarDuplicated,name.c_str());
+    else
+      compileError(EAlreadyAsProc,name.c_str());
+  }
+  // なければ識別子名name、種別vclassの変数を生成し、与えられた記号表に登録
+  VarEntry *var = new VarEntry( vc, name, type, array, size );
+  table->insert(make_pair(name, var));
+  return var;
+}
 
 ProcEntry *addProcedure(string name, Type type, ParamList *params)
 {
